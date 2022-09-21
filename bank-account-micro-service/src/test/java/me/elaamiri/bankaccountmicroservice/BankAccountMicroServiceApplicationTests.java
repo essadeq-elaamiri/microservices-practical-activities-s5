8f4d0c23c0bf760d1407c1c26482e84eaa2c910d
essadeq-elaamiri <essadeq0701@gmail.com>
2022-09-21 09:46:16 +0100

    init activity 1

---
+++ bank-account-micro-service/src/test/java/me/elaamiri/bankaccountmicroservice/BankAccountMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elaamiri.bankaccountmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
