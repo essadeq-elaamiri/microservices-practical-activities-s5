cabd98200c32907e7f54eaf73ac6c2d0ea6d5949
essadeq-elaamiri <essadeq0701@gmail.com>
2022-09-26 01:18:40 +0100

    service without DTOs

--- bank-account-micro-service/src/main/java/me/elaamiri/bankaccountmicroservice/services/BankAccountService.java
+++ bank-account-micro-service/src/main/java/me/elaamiri/bankaccountmicroservice/services/BankAccountService.java
@@ -1,4 +1,9 @@
 package me.elaamiri.bankaccountmicroservice.services;
 
+import me.elaamiri.bankaccountmicroservice.entities.BankAccount;
+import me.elaamiri.bankaccountmicroservice.enumerations.AccountType;
+
 public interface BankAccountService {
+    // Without DTO
+    public BankAccount insertAccount(Double initialBalance, AccountType accountType);
 }
--- bank-account-micro-service/src/main/java/me/elaamiri/bankaccountmicroservice/services/BankAccountServiceImp.java
+++ bank-account-micro-service/src/main/java/me/elaamiri/bankaccountmicroservice/services/BankAccountServiceImp.java
@@ -1,4 +1,19 @@
 package me.elaamiri.bankaccountmicroservice.services;
 
-public class BankAccountServiceImp {
+import me.elaamiri.bankaccountmicroservice.entities.BankAccount;
+import me.elaamiri.bankaccountmicroservice.enumerations.AccountType;
+import me.elaamiri.bankaccountmicroservice.repositories.BankAccountRepository;
+
+public class BankAccountServiceImp implements BankAccountService {
+    BankAccountRepository bankAccountRepository;
+    public BankAccountServiceImp(BankAccountRepository bankAccountRepository){
+        this.bankAccountRepository  = bankAccountRepository;
+    }
+    @Override
+    public BankAccount insertAccount(Double initialBalance, AccountType accountType) {
+        /*
+        Doing some 'metier' code
+         */
+        return null;
+    }
 }
